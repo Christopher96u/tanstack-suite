30c584df33ce8a71767e6e98c4574a62fcf92710
Christopher96u <christophermenendezmorales@gmail.com>
2024-09-15 21:48:14 +1000

    added localstoreprovider to Providers component

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,5 +1,6 @@
 import { queryClient } from "@/config/queryClient";
 import { router } from "@/config/router";
+import { LocalStoreProvider } from "@/stores/local";
 import { SessionStoreProvider } from "@/stores/session";
 
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -8,12 +9,14 @@ import { RouterProvider } from "@tanstack/react-router";
 const Providers = () => {
   // Maybe: get Zustand setter to store token
   return (
-    <SessionStoreProvider>
-      <QueryClientProvider client={queryClient}>
-        {/* other providers */}
-        <RouterProvider defaultPreload="intent" router={router} />
-      </QueryClientProvider>
-    </SessionStoreProvider>
+    <LocalStoreProvider>
+      <SessionStoreProvider>
+        <QueryClientProvider client={queryClient}>
+          {/* other providers */}
+          <RouterProvider defaultPreload="intent" router={router} />
+        </QueryClientProvider>
+      </SessionStoreProvider>
+    </LocalStoreProvider>
   );
 };
 
